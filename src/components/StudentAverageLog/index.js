78ac5051650fb39f22e1186c688b7ee8a7df1f9c
Nick Ray <nickrray@gmail.com>
2020-08-11 09:47:43 -0500

    :construction: Import useState and create students piece of state

--- src/components/StudentAverageLog/index.js
+++ src/components/StudentAverageLog/index.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { AddStudentForm as Form } from './AddStudentForm'
 import { AverageDisplay } from './AverageDisplay'
 import { StudentTable as Table } from './StudentTable'
 
 export const StudentAverageLog = () => {
+  const [students, setStudents] = useState([])
 
   return (
     <div>
