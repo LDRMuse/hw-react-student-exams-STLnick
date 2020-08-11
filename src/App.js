fbc1d109937afe3d667a7684d4bb872ef212c972
Nick Ray <nickrray@gmail.com>
2020-08-11 09:35:09 -0500

    :bulb: Add comments to guide development

--- src/App.js
+++ src/App.js
@@ -6,3 +6,16 @@ export const App = () => {
     <h1>Hello World!</h1>
   );
 }
+
+// TODO
+// Create a table with 3 columns: 'First Name', 'Last Name', 'Exam Score'
+
+// Create a form to add a student and a score (2 text inputs, 1 number input).
+
+// Add some students.
+
+// As you add the students, log the average exam score for all of the students - 'a class average.'
+
+// If the 'class average' is below 70, apply some type of 'special Bulma text class.'
+
+// Do the same thing with any individual student - if score is below 70...
